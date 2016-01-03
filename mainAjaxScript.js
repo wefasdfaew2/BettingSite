228a2d3773a868dfb4317fe61641689c43ac8310
Kristian Nørgaard Jensen <Kristian Nrgaard Jensen>
2016-01-03 15:46:17 +0100

    SignUp Functions

---
+++ mainAjaxScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+            $(".browse").click(function(){
+                $('.ui.labeled.icon.sidebar').sidebar('toggle');
+            });
+        }); 
+
+var x = document.getElementById.bind(document);
+
+x('signUp').onclick = function () {
+    window.location = "file:///C:/Users/Pink%20Jack/BettingSite/signUp.html";
+}
